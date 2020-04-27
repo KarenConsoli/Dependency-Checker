a4023d80d9767255eca8f119f6c18d720a8a033a
Karen Brenda Consoli <kbc_95@hotmail.com>
2020-04-27 17:44:48 -0300

    Build DockerFile

---
+++ DependencyCheckerContainerGenerator/ContainerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyCheckerContainerGenerator
+{
+    class ContainerModel
+    {
+        public string ContainerID { get; set; }
+
+        public string Image { get; set; }
+
+        public string Command { get; set; }
+
+        public string Created { get; set; }
+
+
+    }
+}
---
+++ DependencyCheckerContainerGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyCheckerContainerGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
